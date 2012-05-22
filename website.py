3656db819f5444da64106a3d56e661a0a7c489bb
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-05-22 20:18:09 +0100

    [ADD] Blog schema

--- website.py
+++ website.py
@@ -1,10 +1,39 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
+import sqlite3
+from flask import \
+Flask, \
+request, \
+session, \
+g, \
+redirect, \
+url_for, \
+abort, \
+render_template, \
+flash
+
+# Configuration.
+DATABASE = "/tmp/website.db"
+DEBUG = True
+SECRET_KEY = "huge_secret"
+USERNAME = "admin"
+PASSWORD = "default"
+
+app = Flask( __name__ )
+app.config.from_object( __name__ )
+
+def connect_db():
+    return sqlite3.connect( app.config[ "DATABASE" ] )
 
 @app.route('/')
 def index():
     return render_template( "index.html" )
 
+@app.route( "/blog/" )
+def blog():
+    return "Blog home"
+
+@app.route( "/blog/<int:post_id>" )
+def blog_post( post_id ):
+    return "Blog post %d" % ( post_id, )
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()

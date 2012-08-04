d71c308d86f798505de784e59180c2e0026fd27a
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-08-04 02:41:20 +0100

    [ADD] Doing a deploy from latest code

--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
-sudo apt-get install python-setuptools sqlite3 rubygems 
+sudo apt-get install python-setuptools python-pygments sqlite4 rubygems 
 sudo easy_install pip 
 sudo easy_install --upgrade pytz
 sudo pip install Flask Frozen-Flask Flask-FlatPages 

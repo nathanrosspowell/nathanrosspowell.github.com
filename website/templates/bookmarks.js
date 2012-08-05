3cbbbc7e57e71d436aed74b240a6f73b341937dd
Nathan Ross Powell <nathanrosspowell@gmail.com>
2012-08-05 06:41:01 +0100

    [ADD] Doing a deploy from latest code

---
+++ website/templates/bookmarks.js
@@ -0,0 +1,6 @@
+(function() {
+var sb = document.createElement("script"); sb.type = "text/javascript";sb.async = true;
+sb.src = ("https:" == document.location.protocol ? "https://dtym7iokkjlif.cloudfront.net" : "http://cdn.shareaholic.com") + "/media/js/jquery.shareaholic-publishers-sb.min.js";
+var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(sb, s);
+})();
+
---
+++ website/templates/disqus_comment_thread.js
@@ -0,0 +1,9 @@
+var disqus_shortname = 'nathanrosspowell';
+var disqus_url = "http://www.nathanrosspowell.com{{ page_id }}";
+var disqus_identifier = "{{ page_id }}";
+var disqus_title = "{{ page_title }}";
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();

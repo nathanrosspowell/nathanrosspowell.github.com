6ea9054d049ff3c1496f34e0f812030d067e5f71
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-06-26 22:04:22 -0400

    Updates

--- static/js/shareholic.js
+++
@@ -1,2 +0,0 @@
-var SHRSB_Settings = {"shr_class":{"src":"/static/png","link":"","service":"40,5,7,304,88,61,313","apikey":"0747414f47c2b684cf5480c36b2478689","localize":true,"shortener":"bitly","shortener_key":"","designer_toolTips":true,"tip_bg_color":"black","tip_text_color":"white","twitter_template":"${title} - ${short_link} via @Shareaholic"}};
-var SHRSB_Globals = {"perfoption":"1"};
--- static/js/todo.js
+++
@@ -1,1 +0,0 @@
-$("#result").load("https://raw.github.com/gist/3262124/2b6687195afdca138016fee10adf9a81836b96e9/todo.json")

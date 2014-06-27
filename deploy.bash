fba0d16cd435858bb0fdb0ede0e0e09f65ec6bf2
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-06-26 22:45:47 -0400

    Deploy script now pushes

--- deploy.bash
+++ deploy.bash
@@ -1,4 +1,12 @@
+#usr/bin/env sh
+
+# Use freeze to make the new static site.
 ./freeze.py
+
+# Add all the new files, commit and push them.
 git add --all
 git commit -m "$@"
+git push
+
+# Deploy the new site with grunt.
 grunt
